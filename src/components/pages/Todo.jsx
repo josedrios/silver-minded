01cb890b172a5fe890ca57dc23e8a43c136bec33
josedrios <joserios0531@gmail.com>
2025-03-31 16:02:45 -0700

    Basic progress bars were implemented

--- src/components/pages/Todo.jsx
+++ src/components/pages/Todo.jsx
@@ -1,4 +1,7 @@
-import { TerminalContainer, SubTerminalContainer } from "../layout/TerminalContainer";
+import {
+  TerminalContainer,
+  SubTerminalContainer,
+} from "../layout/TerminalContainer";
 
 export default function Todo() {
   return (
@@ -6,24 +9,52 @@ export default function Todo() {
       id="todo-container"
       classname="full-terminal-container"
       color="red"
-      labels={["42",'cats', "completions"]}
+      labels={["Categories", "Past"]}
       controllers={["note", "complete", "edit", "kill"]}
-      divider={[2, 3]}
+      divider={[1, 3]}
     >
-      <TaskList />
-      
+      <div>
+        <TaskProgressBars />
+      </div>
       <SubTerminalContainer
-      classname="todo-sub"
-      color="red"
-      labels={["Name",'Task', "Tag", 'Status']}
-      divider={[2]}
-      >
-        Test
-      </SubTerminalContainer>
+        classname="todo-sub"
+        color="red"
+        labels={["Name", "Task", "Tag", "Status"]}
+        divider={[2]}
+      ></SubTerminalContainer>
     </TerminalContainer>
   );
 }
 
-function TaskList() {
-  return <div className="tasklist"></div>;
+function TaskProgressBars() {
+  return (
+    <div className="cat-progress-bar-container">
+      <CatProgressBar color="rgb(34, 156, 34)" label="Done" />
+      <CatProgressBar color="rgb(34, 156, 34)" label="New" />
+      <CatProgressBar color="rgb(34, 156, 34)" label="Old" />
+      <CatProgressBar color="rgb(34, 156, 34)" label="Urgent" />
+    </div>
+  );
+}
+
+function CatProgressBar({ color, label }) {
+  const rgb = color.match(/\d+/g).join(",");
+  const boxes = Array.from({ length: 10 });
+  return (
+    <div className="cat-progress-bar">
+      <p className="cat-progress-label">{label}</p>
+      <div className="cat-progress-units-container">
+        {boxes.map((_, i) => (
+          <div
+            key={i}
+            className="cat-progress-unit"
+            style={{
+                backgroundColor: `rgba(${rgb}, ${0.55 + i * 0.05})`
+            }}
+          />
+        ))}
+      </div>
+      <p className="cat-progress-percentage">100%</p>
+    </div>
+  );
 }
