d624463f012829c8bb42a38ec609ede06374397e
josedrios <joserios0531@gmail.com>
2025-03-31 00:03:27 -0700

    Added basic project setup like sass and project structure

---
+++ src/components/layout/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+    return (
+        <nav>
+            <a href="">TODO</a>
+            <a href="">CALENDAR</a>
+            <a href="">THOUGHTS</a>
+            <a href="">TRAINING</a>
+            <a href="">OTHER</a>
+        </nav>
+    )
+}
