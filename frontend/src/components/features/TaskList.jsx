e2c621f0257d79947ac83e0edcaa3481af102af6
josedrios <joserios0531@gmail.com>
2025-04-02 12:06:03 -0700

    Task creation form inputs have been implemented

--- frontend/src/components/features/TaskList.jsx
+++ frontend/src/components/features/TaskList.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef } from "react";
+
 export function TaskList() {
   return (
     <div id="task-list">
@@ -31,10 +33,62 @@ function TaskRow() {
 }
 
 export function TaskCreation() {
+  // Task object data
+  const [taskForm, setTaskForm] = useState({
+    name: "",
+    info: "",
+    tag: "",
+  });
+  // Holds current input text
+  const [currentInput, setCurrentInput] = useState("name");
+  // Holds the current input type (name, info, tag)
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
+
+  // Stay focused on input tag even after submission
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [currentInput]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTaskForm((prev) => ({
+      ...prev,
+      [currentInput]: inputValue,
+    }));
+  };
+
+  useEffect(() => {
+    // Reset input when moving to the next
+    if (taskForm.name === "") {
+      setCurrentInput("name");
+      setInputValue("");
+    } else if (taskForm.info === "") {
+      setCurrentInput("info");
+      setInputValue("");
+    } else if (taskForm.tag === "") {
+      setCurrentInput("tag");
+      setInputValue("");
+    } else {
+      console.log('Task created')
+      console.log(taskForm)
+      setInputValue("");
+      setCurrentInput("name")
+    }
+  }, [taskForm]);
+
   return (
     <div id="task-creation">
-      <span>/task &gt;</span>
-      <input id="task-creation-input" type="text" />
+      <span>/task/{currentInput} &gt;</span>
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={inputRef}
+          className="task-creation-input"
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
