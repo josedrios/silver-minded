ba28556f83b3beb00b6a23190d38e85810fd1d0c
josedrios <joserios0531@gmail.com>
2025-05-13 17:41:41 -0700

    Added an svg that will serve as the tree branch connector to child trees and nodes

--- frontend/src/features/trees/components/TreePage.jsx
+++ frontend/src/features/trees/components/TreePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchTree, TreeHeader } from '../';
 import { SlashLoader, PlusIcon } from '../../../components';
@@ -17,8 +17,8 @@ export default function TreePage({}) {
 
   return (
     <div className="tree-page">
-      {tree ? <TreeHeader tree={tree} setTree={setTree}/> : <SlashLoader />}
-      <div className='tree-children-container'>
+      {tree ? <TreeHeader tree={tree} setTree={setTree} /> : <SlashLoader />}
+      <div className="tree-children-container">
         <TreeChildCard />
       </div>
     </div>
@@ -26,15 +26,54 @@ export default function TreePage({}) {
 }
 
 function TreeChildCard() {
+  const svgRef = useRef(null);
+  const [svgHeight, setSvgHeight] = useState(0);
+
+  useEffect(() => {
+    if (svgRef.current) {
+      setSvgHeight(svgRef.current.clientHeight - 2);
+    }
+  }, []);
+  
   return (
-    <div className="tree-child-card">
-      <div className="tree-child-branch">
-        TB
-      </div>
-      <div className='tree-child-body'>
-        <button><PlusIcon /></button>
-        <div className='tree-child-content'>CONTENT</div>
+    <div className="tree-child-card" ref={svgRef}>
+      <svg width="40">
+        {/* Main vertical line */}
+        <line
+          x1="19"
+          y1="0"
+          x2="19"
+          y2="40"
+          stroke="var(--accent-800)"
+          strokeWidth="2"
+        />
+
+        {/* Optional vertical extension */}
+        <line
+          x1="19"
+          y1="40"
+          x2="19"
+          y2={svgHeight}
+          stroke="var(--accent-800)"
+          strokeWidth="2"
+        />
+
+        {/* Horizontal connector */}
+        <line
+          x1="19"
+          y1="40"
+          x2="39"
+          y2="40"
+          stroke="var(--accent-800)"
+          strokeWidth="2"
+        />
+      </svg>
+      <div className="tree-child-body">
+        <button className="add-child-button">
+          <PlusIcon />
+        </button>
+        <div className="tree-child-content"></div>
       </div>
     </div>
-  )
+  );
 }
